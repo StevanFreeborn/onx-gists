1e5bf050a69c44744012212b16032b38fdbfdfbb
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-08-25 12:54:59 -0500

    chore: remove aria labels

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -27,7 +27,6 @@ export default function Navbar() {
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg
                 className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 20 20"
@@ -53,13 +52,10 @@ export default function Navbar() {
             <button
               onClick={() => setIsNavBarOpen(!isNavbarOpen)}
               type="button"
-              aria-controls="navbar-search"
-              aria-expanded="false"
               className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
             >
               <svg
                 className="w-5 h-5"
-                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 20 20"
@@ -78,13 +74,10 @@ export default function Navbar() {
               onClick={() => setIsNavBarOpen(!isNavbarOpen)}
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-search"
-              aria-expanded="false"
             >
               <span className="sr-only">Open main menu</span>
               <svg
                 className="w-5 h-5"
-                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 17 14"
@@ -109,7 +102,6 @@ export default function Navbar() {
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg
                 className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 20 20"
