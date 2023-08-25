1d9ec0c0ded7c4fed125dc0c53508afd275e7539
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-08-25 09:22:46 -0500

    fix: check for undefined api url

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const session = useUserSession();
 
   async function callApi() {
-    const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL, {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL ?? '', {
       headers: {
         Authorization: `Bearer ${session.data?.apiJwt}`,
       },
